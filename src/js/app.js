82c906c3cd55ae2f2527ea5a21dbfee261159bae
Adrian <adrian_zinko@op.pl>
2020-10-04 00:23:30 +0200

    feat: create brand section

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
 import '../scss/app.scss';
 import 'bootstrap';
 // Your JS Code goes here
+$('.brand-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
